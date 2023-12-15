92782dcb64405c5e3c793aa4e35e4b06c0b9c2ce
JustinasJadeska <justinas.jadeska@gmail.com>
2023-12-15 13:29:02 +0200

    Loading design added

--- Baigiamasis darbas/src/components/pages/myProfile/MyProfile.jsx
+++ Baigiamasis darbas/src/components/pages/myProfile/MyProfile.jsx
@@ -1,6 +1,7 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import UsersContext from "../../../contexts/UsersContext";
+import FormikInput from "../../UI/input/FormikInput";
 
 const StyledProfile = styled.div`
     min-height: 100vh;
@@ -16,7 +17,7 @@ const StyledProfile = styled.div`
 
     .user {
         display: flex;
-        align-items: flex-start;
+        align-items: center;
         justify-content: center;
         gap: 20px;
 
@@ -24,11 +25,28 @@ const StyledProfile = styled.div`
             width: 250px;
         }
     }
+
+    .info {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        > p {
+            margin: 10px 0;
+        }
+    }
+
+    .loading {
+        min-height: 100vh;
+        color: white;
+        background-color: #191919;
+    }
 `
 
 const MyProfile = () => {
 
     const {users, setUsers, loggedInUser, UsersActionTypes} = useContext(UsersContext);
+    // const [aboutMe, setAboutMe] = useState("");
 
     useEffect(() => {
         fetch(`http://localhost:8080/users`)
@@ -44,8 +62,34 @@ const MyProfile = () => {
     const currentUser = users.find(user => user.id === loggedInUser.id)
     
     if(!currentUser){
-        return <div>Loading...</div>
+        return <div className="loading">Loading...</div>
     }
+
+    // const aboutMeChange = (event) => {
+    //     setAboutMe(event.target.value)
+    // }
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault()
+
+    //     const updateUser = {...currentUser, aboutMe}
+
+    //     fetch(`http://localhost:8080/users/${loggedInUser.id}`,{
+    //         method: "PATCH",
+    //         headers: {
+    //             "Content-Type":"application/json"
+    //         },
+    //         body: JSON.stringify(updateUser)
+    //     })
+    //     .then(res => res.json())
+    //     .then(updateUser => {
+    //         setUsers({
+    //             type: UsersActionTypes.get_all,
+    //             data: updateUser
+    //         })
+    //     })
+
+    // }
 
     return ( 
         <StyledProfile>
@@ -54,9 +98,18 @@ const MyProfile = () => {
                 <div>
                     <img src={currentUser.image} alt='Profile picture' />
                 </div>
-                <div>
+                <div className="info">
                     <p>Username: {currentUser.name}</p>
                     <p>Email: {currentUser.email}</p>
+                    {/* <p>About me: </p>
+                    <textarea
+                    value={aboutMe}
+                    placeholder="Tell us about yourself..."
+                    rows={4}
+                    onChange={aboutMeChange}
+                    >
+                    </textarea><br />
+                    <button onClick={handleSubmit}>Save</button> */}
                 </div>
             </div>
         </StyledProfile>
